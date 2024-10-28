a96ed9a2573000ef29d8a74eebcc594313c0fe0c
DriftWebs <julanogamer2003@gmail.com>
2024-10-28 15:18:53 -0300

    Arreglos del regalo 5 y preparacion para regalo 6

--- galeria.js
+++ galeria.js
@@ -1,5 +1,5 @@
 // Número total de imágenes en la carpeta galeria
-const totalImages = 16; // Ajusta este número según la cantidad de imágenes
+const totalImages = 21; // Ajusta este número según la cantidad de imágenes
 
 // Función para cargar las imágenes de la carpeta galeria
 function loadGalleryImages() {
